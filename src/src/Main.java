0cc27c5069f8546d31c5bd0cad9894c28d66bfb1
mathoumin <maxime.thoumin@etu.uca.fr>
2024-10-09 17:19:52 +0200

    Fin propre du programme

--- src/src/Main.java
+++ src/src/Main.java
@@ -13,11 +13,9 @@ public class Main {
 
     // Affiche le menu principal à l'écran.
     private static void printMenu() {
-        System.out.println("\nDe quelle ville voulez vous la météo ?");
+        System.out.println("\nDe quelle ville voulez vous la météo ? Taper exit pour quitter.\n");
         System.out.print("Votre choix : ");
     }
-
-    // Lit un stream
 
 
     public static void main(String[] argv) {
@@ -30,24 +28,27 @@ public class Main {
             String urlPart1 = "https://api.openweathermap.org/data/2.5/weather?q=";
             String urlPart2 = "&appid=7ee0dd93945ae31985996d2cf2d1d95d&exclude=minutely,hourly,daily,alerts&units=metrics";
 
+            String ville = "";
+
             // Boucle principale du programme
-            while (true) {
+            while (!ville.equals("exit")) {
 
                 // Affichage du menu
                 printMenu();
-                String ville = scanner.nextLine();
+                ville = scanner.nextLine();
 
                 // Traitement du choix de l'utilisateur
+                if (!ville.equals("exit")) {
+                    // Création de l'URL complète
+                    URL url = new URL(urlPart1 + ville + urlPart2);
 
-                // Création de l'URL complète
-                URL url = new URL(urlPart1 + ville + urlPart2);
+                    // Récupération des informations
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 
-                urlConnection = (HttpURLConnection) url.openConnection();
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-
-                // Lecture du stream
-                StreamReader.readStream(in);
-
+                    // Lecture du stream
+                    StreamReader.readStream(in);
+                }
             }
 
         } catch (Exception e){
