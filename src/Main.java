54d1d87423de0524f52f116496e44c6bd83f2d4f
mathoumin <maxime.thoumin@etu.uca.fr>
2024-10-09 20:14:13 +0200

    Affichage de la température

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 public class Main {
 
     //
+    public static String ville = "";
     public static boolean validCityBoolean = false;
-
     public static boolean invalidCityBoolean = false;
 
 
@@ -34,7 +34,7 @@ public class Main {
             String urlPart1 = "https://api.openweathermap.org/data/2.5/weather?q=";
             String urlPart2 = "&appid=7ee0dd93945ae31985996d2cf2d1d95d&exclude=minutely,hourly,daily,alerts&units=metric";
 
-            String ville = "";
+
 
             // Boucle principale du programme
             while (!ville.equals("exit")) {
--- src/StreamReader.java
+++ src/StreamReader.java
@@ -1,22 +1,19 @@
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 
 public class StreamReader {
 
     // Affichage du stream dans la console
     public static void readStream(InputStream inputStream) throws IOException {
 
-        // Initialisation
-        StringBuilder resultStringBuilder = new StringBuilder();
-
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                resultStringBuilder.append(line).append("\n");
-            }
+        try (JsonReader jsonReader = Json.createReader(new InputStreamReader(inputStream))) {
+            JsonObject jsonObject = jsonReader.readObject();
+            JsonObject main = jsonObject.getJsonObject("main");
+            System.out.println("Température à " + Main.ville + ": " + main.getJsonNumber("temp").doubleValue() + "°C");
         }
-        System.out.println(resultStringBuilder);
     }
 }
