343629dd9dc4d6cd4850be77be41c5311e440f4e
mathoumin <maxime.thoumin@etu.uca.fr>
2024-10-09 19:28:55 +0200

    Restructuration du projet

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,7 @@ public class Main {
 
             // Initialisation des parties de l'URL
             String urlPart1 = "https://api.openweathermap.org/data/2.5/weather?q=";
-            String urlPart2 = "&appid=7ee0dd93945ae31985996d2cf2d1d95d&exclude=minutely,hourly,daily,alerts&units=metrics";
+            String urlPart2 = "&appid=7ee0dd93945ae31985996d2cf2d1d95d&exclude=minutely,hourly,daily,alerts&units=metric";
 
             String ville = "";
 
